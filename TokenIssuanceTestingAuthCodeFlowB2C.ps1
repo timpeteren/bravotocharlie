260b666f036127394e18e0b2c10076b0f7230da5
Tim Peter Edstrøm <tim.peter.edstrom@amesto.no>
2022-04-27 18:33:44 +0200

    Add auth code grant with PKCE

--- TokenIssuanceTestingAuthCodeFlowB2C.ps1
+++ TokenIssuanceTestingAuthCodeFlowB2C.ps1
@@ -1,24 +1,26 @@
 #############################################################
-# Get OIDC Code from B2C using custom policies
-# App referenced must be a B2C application (not Azure AD)
+# Authorization code grant AAD B2C using custom policies
+# App registration must be a B2C application (not Azure AD)
 #############################################################
+
+############################################################################
+# Authorization code grant with Proof Key for Code Exchange snippets below
+############################################################################
 
 Add-Type -AssemblyName System.Web
 
 # Your Client ID and Client Secret obtained when registering your WebApp
-$tenantName = 'tenant-name.onmicrosoft.com'
-$clientId = 'xxxxx'
-$apiClientId = 'xxxxx'
-$clientSecret = ''  #  // NOTE: Only required for web apps
-$b2cPolicyName = 'b2c_1_signupsignin'
-$resource =  'https://{0}/{1}/user_impersonation' -f $tenantName, $apiClientId # https://{0}/{1}/write
+$tenantName = 'xxx.onmicrosoft.com'
+$clientId = ''
+# $apiClientId = 'xxxxx'
+$clientSecret = ''  #  // NOTE: Only required for web apps bus NOT needed for single-page apps and PKCE 
+$b2cPolicyName = 'b2c_1a_'
+# $resource =  'https://{0}/{1}/user_impersonation' -f $tenantName, $apiClientId # https://{0}/{1}/write
 $redirectUri = 'https://jwt.ms/'
-$scope = 'openid offline_access ' + $resource
+$scope = 'openid profile offline_access ' + $resource
 
 # UrlEncode the scope parameter because it contains the resource URL
 $scopeEncoded = [System.Web.HttpUtility]::UrlEncode($scope)
-# No longer required to encode redirect URI
-# $redirectUriEncoded =  [System.Web.HttpUtility]::UrlEncode($redirectUri)  
 
 Function Get-AuthCode {
     Add-Type -AssemblyName System.Windows.Forms
@@ -46,14 +48,12 @@ Function Get-AuthCode {
 }
 
 $BaseUri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/oauth2/v2.0/authorize" -f $tenantName
+$result = $null
 $url = "$($BaseUri)?" + `
         "client_id=$($clientId)" + `
         "&response_mode=query" + `
         "&response_type=code" + `
         "&redirect_uri=$($redirectUri)" + `
-        # The redirect_uri of your app, where authentication responses can be sent and received by your app. It must exactly match one of the redirect_uris you registered in the portal, 
-        # except it must be url encoded. For native & mobile apps, you should use the default value of https://login.microsoftonline.com/common/oauth2/nativeclient
-        # no longer required to encode redirect_uri         "&redirect_uri=$($redirectUriEncoded)" + `
         "&scope=$($scopeEncoded)" + `
         "&p=$($b2cPolicyName)" + `
         "&state=myState" + `
@@ -73,7 +73,7 @@ Write-Output $result
 
 $GrantType = "authorization_code"
 $Uri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/$($b2cPolicyName)/oauth2/v2.0/token" -f $tenantName
-
+$token = $null
 $Body = @{
     "grant_type" = $GrantType
     "client_id" = $clientid
@@ -89,10 +89,9 @@ Write-Output $token
 #######################
 #### refresh_token ####
 #######################
-
-$Uri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/$($b2cPolicyName)/oauth2/v2.0/token" -f $tenantName
 $GrantType = "refresh_token"
-
+$Uri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/$($b2cPolicyName)/oauth2/v2.0/token" -f $tenantName
+$refreshtoken = $null
 $Body = @{
     "grant_type" = $GrantType
     "client_id" = $clientid
@@ -116,3 +115,93 @@ $logoutUrl =
 "&post_logout_redirect_uri=$($redirectUri)"
 
 Invoke-WebRequest -Uri $logoutUrl -Method Get
+
+
+#############################################################
+# Authorization code grant with Proof Key for Code Exchange
+# Currently *only* employs 'plain' code_challenge_method
+# Helper methods included to properly format code_challenge
+#############################################################
+
+# Helper methods
+function Get-Base64UrlEncode {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [byte[]]
+        $Bytes
+    )
+
+    [System.Convert]::ToBase64String($bytes).TrimEnd("=").Replace('+', '-').Replace('/', '_')
+}
+
+function Get-SHA256 {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $String
+    )
+
+    $hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+    $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))
+
+    #$hashString = [System.BitConverter]::ToString($hash)
+    #$hashString.Replace('-', '')
+}
+
+# Generate code challenge
+$code_challenge_original = (New-Guid).ToString()
+$code_challenge = Get-Base64UrlEncode (Get-SHA256 $code_challenge_original)
+
+$BaseUri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/oauth2/v2.0/authorize" -f $tenantName
+$result = $null
+$url = "$($BaseUri)?" + `
+        "client_id=$($clientId)" + `
+        "&response_mode=query" + `
+        "&response_type=code" + `
+        "&redirect_uri=$($redirectUri)" + `
+        "&scope=$($scopeEncoded)" + `
+        "&p=$($b2cPolicyName)" + `
+        "&state=myState" + `
+        "&code_challenge=$code_challenge" + `
+        "&code_challenge_method=plain" + `
+        "&nonce=1234randomkr0234kfa12"
+
+$result = Get-AuthCode
+Write-Output $result
+
+###############################################
+# Exchange Code for id_token and refresh_token
+###############################################
+
+$GrantType = "authorization_code"
+$Uri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/$($b2cPolicyName)/oauth2/v2.0/token" -f $tenantName
+$token = $null
+$Body = @{
+    "grant_type" = $GrantType
+    "client_id" = $clientid
+    "code" = $result.code
+    "scope" = $scope
+    "redirect_uri" = $redirectUri
+    "code_verifier" = $code_challenge
+}
+
+$token = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body
+Write-Output $token
+
+#######################
+#### refresh_token ####
+#######################
+
+$GrantType = "refresh_token"
+$Uri = "https://$($tenantName.Split('.')[0]).b2clogin.com/{0}/$($b2cPolicyName)/oauth2/v2.0/token" -f $tenantName
+$refreshtoken = $null
+$Body = @{
+    "grant_type" = $GrantType
+    "client_id" = $clientid
+    "refresh_token" = $token.refresh_token
+    "scope" = $scope
+    "redirect_uri" = $redirectUri
+}
+
+$refreshtoken = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body
+Write-Output $refreshtoken
